d83c1fb995f38a31eb871aca28747e1714a998b9
BigBaz <louisvincent@famillecapelli.fr>
2023-04-18 19:56:42 +0200

    [+] parser

---
+++ pizza_parser.py
@@ -0,0 +1,19 @@
+def parse(path):
+    clients = []
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        clients_nb = int(lines[0])
+        lines = lines[1:]
+        for i in range(clients_nb):
+            liked = lines[2*i].strip().split(' ')[1:]
+            disliked = lines[2*i+1].strip().split(' ')[1:]
+            clients.append((liked, disliked))
+    return clients
+
+
+if __name__ == '__main__':
+    print(len(parse('data/a_exemple.txt')))
+    print(parse('data/a_exemple.txt'))
+    print(len(parse('data/b_basique.txt')))
+    print(parse('data/b_basique.txt'))
+        
