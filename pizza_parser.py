9152e2a2edd0dcbe5346ca5efbf5da986999f7f8
BigBaz <louisvincent@famillecapelli.fr>
2023-04-19 00:07:07 +0200

    [+] using Client class

--- pizza_parser.py
+++ pizza_parser.py
@@ -1,3 +1,5 @@
+from client import Client
+
 def parse(path):
     clients = []
     with open(path, 'r') as f:
@@ -7,13 +9,15 @@ def parse(path):
         for i in range(clients_nb):
             liked = lines[2*i].strip().split(' ')[1:]
             disliked = lines[2*i+1].strip().split(' ')[1:]
-            clients.append((liked, disliked))
+            clients.append(Client(liked, disliked))
     return clients
 
 
 if __name__ == '__main__':
     print(len(parse('data/a_exemple.txt')))
-    print(parse('data/a_exemple.txt'))
+    for e in parse('data/a_exemple.txt'):
+        print(e)
     print(len(parse('data/b_basique.txt')))
-    print(parse('data/b_basique.txt'))
+    for e in parse('data/b_basique.txt'):
+        print(e)
         
