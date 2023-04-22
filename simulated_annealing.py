0bd6129c68306462746c8d0a487dd33a4b73f0fb
BigBaz <louisvincent@famillecapelli.fr>
2023-04-22 14:32:51 +0200

    [~] rename param

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -22,11 +22,11 @@ def accept_probability(old_fitness, new_fitness, temperature):
     except ZeroDivisionError:
         return 0
 
-def simulated_annealing(file, temperature_init, annealing_rate=0.99, nb_iter=None, objective=None, output_file=None):
+def simulated_annealing(file, temperature_init, annealing_rate=0.99, max_iterations=None, objective=None, output_file=None):
     if output_file is None:
         output_file = file.split('/')[1]
-    if objective is None and nb_iter is None:
-        raise ValueError("Either nb_iter or objective must be specified")
+    if objective is None and max_iterations is None:
+        raise ValueError("Either max_iterations or objective must be specified")
     state = first_state(file)
     best_state = state
     best_fitness = state.get_score()
@@ -34,7 +34,7 @@ def simulated_annealing(file, temperature_init, annealing_rate=0.99, nb_iter=Non
     count = 0
     if not os.path.exists("solutions/simulated_annealing"):
         os.makedirs("solutions/simulated_annealing")
-    while (nb_iter is None or count < nb_iter) and (objective is None or best_fitness < objective):
+    while (max_iterations is None or count < max_iterations) and (objective is None or best_fitness < objective):
         neighbour = get_one_neighbour(state)
         if neighbour.get_score() > state.get_score():
             state = neighbour
@@ -73,7 +73,7 @@ def plot_results(files, titles=None):
     plt.show()
 
 if __name__ == "__main__":
-    for temp in [100, 50, 20, 10, 5, 2, 1]:
-        for rate in [0.99, 0.95, 0.9, 0.8, 0.5, 0.1]:
-            simulated_annealing("data/d_difficile.txt", temperature_init=temp, annealing_rate=rate, nb_iter=1000, output_file=f"d_difficile_{temp}_{str(rate).split('.')[1]}.txt")
-    plot_results(["d_difficile_100_0.99.txt", "d_difficile_100_0.95.txt", "d_difficile_100_0.9.txt", "d_difficile_100_0.8.txt", "d_difficile_100_0.5.txt"])
+    for temp in [50, 20, 10, 5, 2, 1]:
+        for rate in [0.99, 0.95, 0.9, 0.8]:
+            simulated_annealing("data/d_difficile.txt", temperature_init=temp, annealing_rate=rate, max_iterations=1000, output_file=f"d_difficile_{temp}_{str(rate).split('.')[1]}.txt")
+    # plot_results(["d_difficile_100_0.99.txt", "d_difficile_100_0.95.txt", "d_difficile_100_0.9.txt", "d_difficile_100_0.8.txt", "d_difficile_100_0.5.txt"])
